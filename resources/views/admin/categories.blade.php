38349a3596cd39442067ffcfa71581eb9595724e
mohammedouda <oudah016@gmail.com>
2021-01-07 22:44:41 +0200

    first commit

---
+++ resources/views/admin/categories.blade.php
@@ -0,0 +1,53 @@
+@extends('layout')
+@section('content')
+
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h2>
+                            Categories
+
+                            <a href="categories/create" class="btn btn-default pull-right">Create New</a>
+                        </h2>
+                    </div>
+
+                    <div class="panel-body">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Post Count</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                                @foreach ($category as $sadi)
+                                <tr>
+                                    <td>{{ $sadi->name }}</td>
+                                    <td>{{ $sadi->post_count }}</td>
+                                    <td>
+                                        <a href="categories/{{$sadi->id}}/edit" class="btn btn-xs btn-info">Edit</a>
+                                        <a href="categories/1" data-method="DELETE" data-token="32Mxrb2s2QPyv3C1h4iYcbfZBT7PmU7Tfm9koxkk"
+                                            data-confirm="Are you sure?" class="btn btn-xs btn-danger">Delete</a>
+                                    </td>
+                                </tr>
+
+                                @endforeach
+
+
+
+                            </tbody>
+                        </table>
+
+
+
+                    </div>
+                </div>
+            </div>
+
+    </div>
+@endsection
---
+++ resources/views/admin/edittags.blade.php
@@ -0,0 +1,60 @@
+@extends('layout')
+@section('content')
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                <h2>
+                  edit Tag
+
+                  <a href="/admin/tags" class="btn btn-default pull-right"
+                    >Go Back</a
+                  >
+                </h2>
+              </div>
+
+              <div class="panel-body">
+                <form
+                  method="POST"
+                  action="/admin/tags"
+                  class="form-horizontal"
+                >
+                {{-- <input type="hidden" name="_token" value="{{csrf_token()}}"> --}}
+                {{-- {{ csrf_field() }} --}}
+                @csrf
+                  <div class="form-group">
+                    <label for="name" class="col-md-2 control-label"
+                      >Name</label
+                    >
+
+                    <div class="col-md-8">
+                      <input
+                        class="form-control"
+                        required="required"
+                        autofocus="autofocus"
+                        name="name"
+                        type="text"
+                        id="name"
+                      />
+
+                      <span class="help-block">
+                        <strong></strong>
+                      </span>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="col-md-8 col-md-offset-2">
+                      <button type="submit" class="btn btn-primary">
+                        Update
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+ @endsection
